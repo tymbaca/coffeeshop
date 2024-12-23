30c4c984901f61b66d208811da800e753ee4f5e6
t.rostomyan <t.rostomyan@hldtech.ru>
2024-12-24 01:54:25 +0200

    init

---
+++ waiter/cmd/waiter/main.go
@@ -0,0 +1,15 @@
+package waiter
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
